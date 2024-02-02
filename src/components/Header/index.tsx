7d85439d787bc2af44ca69c3ffef21a46dc60a1f
DominMFD <mfdmatheus@hotmail.com>
2024-02-02 18:22:40 -0300

    main quase finalizada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,27 @@
+'use client';
+
+import * as React from 'react'
 import styles from './styles.module.scss';
 import { Roboto_Mono } from "next/font/google";
+import { useEffect, useState } from 'react';
 
 const roboto = Roboto_Mono({ subsets: ["latin"] });
 
 export default function Header() {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
 
     return (
         <div className={roboto.className}>
