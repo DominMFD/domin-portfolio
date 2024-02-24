ee371a71dee4b97e5f354317cb4740532783574c
DominMFD <mfdmatheus@hotmail.com>
2024-02-24 18:41:11 -0300

    fix: header complete

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.scss';
 import { Roboto_Mono } from "next/font/google";
 import { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../AppContext/AppContext';
-import HeaderDropdown from '../HeaderDropdown';
+import { FiAlignJustify, FiPlus } from 'react-icons/fi';
 
 const roboto = Roboto_Mono({ subsets: ["latin"] });
 
@@ -13,24 +13,38 @@ export default function Header() {
 
  const {windowWidth} = useContext(AppContext)
 
-    if (windowWidth < 580) {
-      return (
-        <div className={roboto.className}>
-          <HeaderDropdown />
-        </div>
-      )
-    }
+ const [isOpen, setIsOpen] = useState<boolean>(false);
+ const [isMobile, setIsMobile] = useState<boolean>(false);
+
+ useEffect(() => {
+    setIsMobile(windowWidth < 580);
+}, [windowWidth]);
+
+ const handleHeadler = () => {
+  setIsOpen(!isOpen)
+}
 
     return (
         <div className={roboto.className}>
-            <nav className={styles.header}>
-                <ul className={styles.navegation}>
+          <div className={styles.header}>
+            <div className={styles.icon}
+            onClick={handleHeadler}>
+              {isOpen ? 
+              <FiPlus color="#9c68b4" size="40px" style={{ rotate: "45deg" }} />
+              :
+              <FiAlignJustify color="#9c68b4" size="40px" />
+              }
+            </div>
+            <nav>
+                <ul className={styles.navegation} style={isMobile ? {display: isOpen ? 'flex': 'none'} : {}}>
                     <li><a href="">Sobre Mim</a></li>
                     <li><a href="">Projetos</a></li>
                     <li><a href="">Formações</a></li>
                     <li><a href="">Contato</a></li>
                 </ul>
             </nav>
+          </div>
+            
         </div>
         
     )
