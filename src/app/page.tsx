d30eb206abcc08d254cae72a367139ae2076fbed
DominMFD <mfdmatheus@hotmail.com>
2024-02-25 15:11:30 -0300

    feat: AboutMe section create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AppContextProvider from "@/components/AppContext/AppContext";
 import styles from "./page.module.css";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
+import AboutMe from "@/components/AboutMe";
 
 export default function Home() {
 
@@ -12,6 +13,7 @@ export default function Home() {
       </header>
       <main>
         <Main />
+        <AboutMe />
       </main>
     </AppContextProvider>
   );
