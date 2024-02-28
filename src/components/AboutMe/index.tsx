76fc6ef4cb62f3988fbaf0b0bdde34afdc829282
DominMFD <mfdmatheus@hotmail.com>
2024-02-28 17:00:18 -0300

    chore: aboutMeCard create

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import ProfilePicture from '../../../public/fotoPerfil.jpg'
 import styles from './styles.module.scss'
+import AboutMeCard from '../AboutMeCard';
 
 export default function AboutMe() {
 
@@ -11,11 +12,11 @@ export default function AboutMe() {
               <div className={styles.image__container}>
                 <Image className={styles.image} src={ProfilePicture} alt='Foto de perfil do Matheus'/>
               </div>
-              <div className={styles.infomation}>
-                <p>
-                Olá, me chamo Matheus Domingos e atualmente sou desenvolvedor Frontend e estou cursando Análise e Desenvolvimento de Sistemas.
-                </p>
-                <p></p>
+              <div className={styles.information}>
+                <AboutMeCard />
+              </div>
+              <div>
+                
               </div>
             </div>
         </section>
---
+++ src/components/AboutMeCard/index.tsx
@@ -0,0 +1,19 @@
+import styles from './styles.module.scss';
+
+export default function AboutMeCard() {
+
+    return (
+       <div className={styles.container}>
+         <p className={styles.content}>
+            Olá, me chamo Matheus Domingos, sou um desenvolvedor Frontend em formação e estudante de Análise e Desenvolvimento de Sistemas. Apesar de estar no início da minha jornada a área do desenvolvimento, venho me dedicando intensamente ao aprendizado e à prática.
+        </p>
+        <p>
+         Através de projetos pessoais e participação em projetos voluntários, tenho desenvolvido minhas habilidades e obtido uma valiosa experiência no mundo da tecnologia. Sou conhecido pela minha organização e foco, mantendo um rigoroso cronograma de estudos e utilizando ferramentas como o Notion para me manter organizado.
+        </p>
+        <p>
+        Estou ansioso para embarcar na minha primeira oportunidade profissional, buscando contribuir para empresas e projetos que tenham um impacto positivo na vida das pessoas através da inovação e da tecnologia.Estou ansioso para embarcar na minha primeira oportunidade profissional, buscando contribuir para empresas e projetos que tenham um impacto positivo na vida das pessoas através da inovação e da tecnologia.
+        </p>
+    </div> 
+    )
+    
+}
