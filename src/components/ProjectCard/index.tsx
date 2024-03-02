d7eb53f7689ea2deed8c14b49c76758b7639e690
DominMFD <mfdmatheus@hotmail.com>
2024-03-02 15:51:15 -0300

    feat: Project card done

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,16 +1,24 @@
 import styles from './styles.module.scss'
 import Image from "next/image";
 import ImagemTeste from '../../../public/Captura de tela 2024-03-01 170146.png'
+import Link from 'next/link';
+import { skillsIcon } from '@/service/skillsIcon';
 
 export default function ProjectCard() {
 
     return (
         <div className={styles.container}>
-           <Image src={ImagemTeste} alt='WebPokeDex' />
-           <div>
+        <Link href='https://dominmfd.github.io/WebPokeDex/' target='_blank'>
+            <div className={styles.image__container}>
+               <Image src={ImagemTeste} alt='WebPokeDex' className={styles.image}/> 
+            </div>
+           <div className={styles.information}>
             <h4 className={styles.title}>WebPokedex</h4>
-            <div className={styles.tecnologies}></div>
+            <div className={styles.tecnologies}>
+                {skillsIcon.Html} {skillsIcon.Css} {skillsIcon.Js}
+            </div>
            </div>
+        </Link>
         </div>
     )
 }
---
+++ src/service/skillsIcon.tsx
@@ -0,0 +1,15 @@
+import { BiLogoTypescript, BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoReact, BiLogoAngular, BiLogoJava } from "react-icons/bi"
+import { SiCsharp } from "react-icons/si";
+
+export const skillsIcon = {
+    Html: <BiLogoHtml5 color="#E44D26" size="50px" />,
+    Css: <BiLogoCss3  color="#006BC0" size="50px" />,
+    Js:  <BiLogoJavascript  color="#F7E018" size="50px" />,
+    Ts:  <BiLogoTypescript  color="#2D79C7" size="50px" />,
+    React: <BiLogoReact  color="#2D79C7" size="50px" />,
+    ReactN: <BiLogoReact  color="#2D79C7" size="50px" />,
+    Angular: <BiLogoAngular  color="#DE002D" size="50px" />,
+    Java: <BiLogoJava color="#F39110" size="50px" />,
+    Csharp: <SiCsharp  color="#876CE3" size="50px" />
+
+}
