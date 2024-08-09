e342d6f9673cacf64a3344bdf4cde37534769fce
DominMFD <mfdmatheus@hotmail.com>
2024-08-08 23:18:59 -0300

    chore: header done

--- src/components/Header/components/Nav/index.tsx
+++ src/components/Header/components/Nav/index.tsx
@@ -5,9 +5,9 @@ export default function Nav() {
   return (
     <nav className={styles.container}>
         <ul className={styles.list}>
-          <li className={styles.item}>Home</li>
-          <li className={styles.item}>Sobre Mim</li>
-          <li className={styles.item}>Projetos</li>
+          <li className={styles.item}>Home <span className={styles.bar}></span></li>
+          <li className={styles.item}>Sobre Mim <span className={styles.bar}></span></li>
+          <li className={styles.item}>Projetos <span className={styles.bar}></span></li>
         </ul>
     </nav>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,34 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
 import styles from './index.module.scss';
 import Logo from "./components/Logo";
 import { PiListBold } from "react-icons/pi";
+import { BsTwitterX, BsGithub, BsLinkedin } from "react-icons/bs";
 import Nav from "./components/Nav";
+import classNames from "classnames";
+import Link from "next/link";
 
 export default function Header() {
+  const [navIsOpen, setNavIsOpen] = useState<boolean>(false);
+
  
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
+      <div className={styles.logo}>
         <Logo />
       </div>
-      <div className={styles.right}>
+      <div className={styles.nav} onClick={() => setNavIsOpen(!navIsOpen)}>
         <PiListBold className={styles.icon}/>
       </div>
-      <div className={styles.mobile}>
+      <div className={classNames(styles.mobile, {[styles.open]: navIsOpen, [styles.close]: !navIsOpen})}>
         <Nav />
-      </div>  
+      </div>
+      <div className={styles.socials}>
+        <Link className={styles.link} href={'https://github.com/DominMFD'} target="_blank"><BsGithub /></Link>
+        <Link className={styles.link} href={'https://www.linkedin.com/in/matheusfdomingos/'} target="_blank"><BsLinkedin /></Link>
+        <Link className={styles.link} href={'https://x.com/Domiinguera14'} target="_bank"><BsTwitterX /></Link>
+      </div>    
     </div>
   )
 }
